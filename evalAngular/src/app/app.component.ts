4ae255f534559b1aaa057f5099cf11a6af6e1d5e
Gislain Quenea <quenea.gislain@gmail.com>
2021-12-10 11:57:52 +0100

    Exercice 4 : Interpolation de variables

--- evalAngular/src/app/app.component.ts
+++ evalAngular/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Statistique } from './models/statistique';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'evalAngular';
+  public tabStat: Statistique[] = [];
+  constructor() {
+    let meteoFrance: Statistique = new Statistique('10', 'meteo france', 20);
+    let meteoItalie: Statistique = new Statistique('11', 'meteo Italie', 25);
+    this.tabStat.push(meteoFrance, meteoItalie);
+  }
 }
--- evalAngular/src/app/models/statistique.ts
+++ evalAngular/src/app/models/statistique.ts
@@ -1,2 +1,10 @@
 export class Statistique {
+  public identifiant: string;
+  public titre: string;
+  public valeur: number;
+  constructor(identifiant: string, titre: string, valeur: number) {
+    this.identifiant = identifiant;
+    this.titre = titre;
+    this.valeur = valeur;
+  }
 }
