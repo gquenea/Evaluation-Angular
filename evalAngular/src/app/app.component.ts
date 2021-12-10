0d0ede58e32df23b8bf3e3f5138f87a669f9b845
Gislain Quenea <quenea.gislain@gmail.com>
2021-12-10 14:34:12 +0100

    Exercice 6 : Liste

--- evalAngular/src/app/app.component.ts
+++ evalAngular/src/app/app.component.ts
@@ -9,9 +9,13 @@ import { Statistique } from './models/statistique';
 export class AppComponent {
   title = 'evalAngular';
   public tabStat: Statistique[] = [];
+
   constructor() {
     let meteoFrance: Statistique = new Statistique('10', 'meteo france', 20);
     let meteoItalie: Statistique = new Statistique('11', 'meteo Italie', 25);
     this.tabStat.push(meteoFrance, meteoItalie);
+    setTimeout(() => {
+      this.tabStat.push(new Statistique('12', 'meteo Allemagne', 23));
+    }, 5000);
   }
 }
